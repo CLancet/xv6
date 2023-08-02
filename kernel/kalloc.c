82a9fd65e9bded7686ee6745be4c734f29f32506
lancet <2385366128@qq.com>
2023-08-02 17:52:27 +0800

    finish Lab2

--- kernel/kalloc.c
+++ kernel/kalloc.c
@@ -80,3 +80,18 @@ kalloc(void)
     memset((char*)r, 5, PGSIZE); // fill with junk
   return (void*)r;
 }
+
+uint64 acquire_freemem(){
+  struct run *r;
+  uint64 number = 0;
+
+  acquire(&kmem.lock);
+  r = kmem.freelist;
+  while(r){
+    r=r->next;
+    number++;
+  }
+  release(&kmem.lock);
+
+  return number*PGSIZE;
+}
